1209bd7ade07e2160fc755f35ce62005de2b6e7d
thaynsworth <thomas.haynsworth@gmail.com>
2015-08-18 23:48:56 -0400

    added GULPPP

---
+++ dist/scripts.js
@@ -0,0 +1,119 @@
+"use strict";
+
+var RESNICK = {
+
+  init: function init() {
+    var router = new RESNICK.router();
+
+    Backbone.history.start();
+  },
+
+  createElement: function createElement(string) {
+    var $selector = $(document.getElementsByClassName(string)),
+        element = document.createElement('div');
+
+    element.className = string;
+    element.dataset.view = string;
+    $selector.remove();
+    $(element).insertAfter(new RESNICK.Container().$el);
+  },
+
+  navActive: function navActive(string) {
+    $('.nav-item').removeClass('nav-active');
+    $('.nav-' + string).addClass('nav-active');
+  },
+
+  changeState: function changeState(string) {
+    this.navActive(string);
+    this.createElement(string);
+  }
+
+};
+
+_.extend(Backbone.View.prototype, RESNICK);
+_.extend(Backbone.Router.prototype, RESNICK);
+RESNICK.Container = Backbone.View.extend({
+
+  el: '#main-container',
+  child: null,
+
+  render: function render() {
+    this.$el.html(this.child.$el);
+
+    return this;
+  }
+
+});
+RESNICK.IndexView = Backbone.View.extend({
+
+  el: '.index',
+
+  viewContainer: _.template($('#index-container-template').html()),
+
+  initialize: function initialize() {
+    this.render();
+  },
+
+  render: function render() {
+    var selector = this.$el,
+        template = this.viewContainer;
+
+    this.setView(selector, template);
+
+    return this;
+  }
+
+});
+
+RESNICK.Router = Backbone.Router.extend({
+
+  wrapper: null,
+  indexView: null,
+  aboutView: null,
+
+  routes: {
+    '': 'index',
+    'about': 'about'
+  },
+
+  initialize: function initialize() {
+    this.wrapper = new RESNICK.Container();
+  },
+
+  index: function index() {
+    this.changeState('index');
+
+    if (this.indexView === null) {
+      this.indexView = new RESNICK.IndexView();
+    }
+
+    this.wrapper.child = this.indexView;
+    this.wrapper.render();
+  }
+
+});
+
+RESNICK.init();
+
+// window.App = {
+//   Models: {},
+//   Collections: {},
+//   Views: {},
+
+//   start: function(data) {
+//     var router = new App.Router();
+
+//     router.on('route:index', function() {
+//       $('#sample-info').append("index route has been called..");
+//     });
+
+//     router.on('route:about', function() {
+//       var aboutView = new About({
+//         collection: aboutCollection
+//       });
+
+//     });
+
+//     Backbone.history.start();
+//   }
+// };
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
-var requireDir = require('require-dir');
+var gulp = require('gulp');
+var concaty = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var gutil = require('gulp-util');
+var babel = require('gulp-babel');
 
-requireDir('./config/tasks', {
-  recurse: true
+var arrayJS = ["assets/js/lib/jquery.js", "assets/js/lib/underscore.js", "assets/js/lib/backbone.js", "assets/js/lib/bootstrap.js", "assets/js/lib/lightbox.js", "assets/js/lib/classie.js", "assets/js/lib/pathLoader.js", "assets/js/RESNICK.js", "assets/js/views/wrapper.js", "assets/js/views/index.js", "assets/js/router.js", "assets/js/main.js"];
+
+gulp.task('default', function() {
+  return gulp.src(arrayJS)
+    .pipe(concaty("scripts.js"))
+    .pipe(babel())
+    .pipe(gulp.dest("dist"))
+    .pipe(uglify()).on('error', gutil.log)
+    .pipe(rename("scripts.min.js"))
+    .pipe(gulp.dest("dist")).on('error', gutil.log);
 });
